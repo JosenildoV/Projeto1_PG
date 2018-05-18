ec018c946a96c7f322eabce2ffa0c78259f9539a
Josenildo Vicente de Araujo <jva@windows.cin.ufpe.br>
2018-05-18 14:27:23 -0300

    Implementação da ligação dos pontos, que estão sendo criados, por uma linha

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('meuCanvas');
 
 var circulo = canvas.getContext('2d');
-
+var linha = canvas.getContext('2d');
 var circulos = [];
 var i=0;
 
@@ -11,8 +11,18 @@ function criarPonto(circuloX, circuloY){
 	circulo.fillStyle = "#000000";
 	circulo.fill();
 	circulo.stroke();
-	circulos.push(circulo);
+	circulos.push([circulo,circuloX,circuloY]);
 	i=i+1;
+	if(circulos.length>1){
+		criarLinha(circulos[i-2],circulos[i-1]);
+	}
+}
+
+function criarLinha(circulo1,circulo2){
+	linha.beginPath();
+	linha.moveTo(circulo1[1],circulo1[2]);
+	linha.lineTo(circulo2[1],circulo2[2]);
+	linha.stroke();
 }
 
 canvas.addEventListener('click', function(e){
