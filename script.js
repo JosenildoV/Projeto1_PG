e412cf5fe0844ce99f64cb9f682486df57a791c7
josenildoV <jva@cin.ufpe.br>
2018-05-18 21:58:09 -0300

    criação de um array para as linhas dos pontos de controle, renomeação das variáveis e funções, e, adição de comentários

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
+//função para mudanças no tamanho do canvas
 function resizeCanvas(width, height) {
     canvas.width = width;
     canvas.height = height;
 }
 
+//função para definir o canvas no tamanho da tela no navegador
 function resizeToFit() {
     var width = parseFloat(window.getComputedStyle(canvas).width);
     var height = parseFloat(window.getComputedStyle(canvas).height);
@@ -17,30 +18,36 @@ var linha = canvas.getContext('2d');
 
 resizeToFit();
 
-var circulos = [];
-var i=0;
+var linhas_controle = [];//Array que guarda as linhas que ligam os pontos de controle
+var pontos_controle = [];//Array que guarda os pontos de controle que são criados
+var qtd_pontos_controle=0;
 
-function criarPonto(circuloX, circuloY){
+//Função que cria os pontos de controle e os guarda no array
+function criarPontoControle(mouseX, mouseY){
 	circulo.beginPath();
-	circulo.arc(circuloX, circuloY, 5, 0, 2 * Math.PI);
+	circulo.arc(mouseX, mouseY, 5, 0, 2 * Math.PI);
 	circulo.fillStyle = "#ff0000";
 	circulo.fill();
 	circulo.stroke();
-	circulos.push([circulo,circuloX,circuloY]);
-	i=i+1;
-	if(circulos.length>1){
-		criarLinha(circulos[i-2],circulos[i-1]);
+	pontos_controle.push([circulo,mouseX,mouseY]);
+	qtd_pontos_controle++;
+	if(pontos_controle.length>1){
+		criarLinhaControle(pontos_controle[qtd_pontos_controle-2],
+					pontos_controle[qtd_pontos_controle-1]);
 	}
 }
 
-function criarLinha(circulo1,circulo2){
+//Função que cria as linhas dara os pontos de controle
+function criarLinhaControle(circulo1,circulo2){
 	linha.beginPath();
 	linha.moveTo(circulo1[1],circulo1[2]);
 	linha.lineTo(circulo2[1],circulo2[2]);
 	linha.strokeStyle = "#ff0000";
+	linhas_controle.push([linha,[circulo1[1],circulo1[2]],[circulo2[1],circulo2[2]]]);
 	linha.stroke();
 }
 
+//Evento de click no canvas para a criação de um ponto de controle
 canvas.addEventListener('click', function(e){
-	criarPonto(e.offsetX, e.offsetY);
+	criarPontoControle(e.offsetX, e.offsetY);
 });
